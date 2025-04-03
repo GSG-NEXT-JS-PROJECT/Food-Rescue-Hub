86a216c9c558cdfbc430cc6c2c2b99693c54429e
m7md-Y-3mra <kingmoha2002@gmail.com>
2025-04-03 16:02:21 +0200

    fix: resolve all issues in login

--- app/api/auth/sign-in/route.ts
+++ app/api/auth/sign-in/route.ts
@@ -1,26 +1,34 @@
 import { NextRequest, NextResponse } from "next/server";
 import AuthService from "@/module/services/auth.service";
-import dbConnect from "@/DB/connection";
+import { createSession } from "@/lib/StoreGetDeleteSession";
 
 export async function POST(req: NextRequest) {
-    await dbConnect();
-    try {
-        const data = await req.json();
-        if (!data || !data.email || !data.password) {
-            return NextResponse.json({ error: "Email and password are required" }, { status: 400 });
-        }
-        
-        const { user, token } = await AuthService.signIn(data);
-        
-        return NextResponse.json({
-            message: "User logged in successfully",
-            user,
-            token
-        }, { status: 200 });
-    } catch (error) {
-        if (error instanceof Error) {
-            return NextResponse.json({ error: error.message }, { status: 401 });
-        }
-        return NextResponse.json({ error: "An unknown error occurred" }, { status: 500 });
+  try {
+    const data = await req.json();
+    if (!data || !data.email || !data.password) {
+      return NextResponse.json(
+        { error: "Email and password are required" },
+        { status: 400 }
+      );
     }
+    
+    const user = await AuthService.signIn(data);
+    const token = await createSession(user.id, user.role);
+    return NextResponse.json(
+      {
+        message: "User logged in successfully",
+        token,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json({ error: error.message }, { status: 401 });
+    }
+
+    return NextResponse.json(
+      { error: "An unknown error occurred" },
+      { status: 500 }
+    );
+  }
 }
--- module/services/auth.service.ts
+++ module/services/auth.service.ts
@@ -4,76 +4,59 @@ import { sendEmail } from "@/lib/sendEmail";
 import { comparePassword, hashPassword } from "@/lib/hashAndCompare";
 import UserRepository from "../repositories/auth.repo";
 import { verificationEmailTemplate } from "@/lib/verificationEmailTemplate";
-import jwt from "jsonwebtoken";
 
 class AuthService {
-    async signUp(data: IUser) {
-        const existingUser = await UserRepository.findUserByEmail(data.email);
-        if (existingUser) {
-            throw new Error("Email is already in use");
-        }
-        const hashedPassword: string = await hashPassword(data.password);
-        const newUser = await UserRepository.createUser(data, hashedPassword);
-        const verificationToken = newUser.getVerificationToken();
-        await newUser.save();
-        const verificationLink = `${process.env.NEXT_PUBLIC_URL}/verify-email?verifyToken=${verificationToken}&id=${newUser?._id}`;
-        const message = verificationEmailTemplate(verificationLink);
-
-        await sendEmail(newUser?.email, "Email Verification", message);
-        return { user: newUser };
+  async signUp(data: IUser) {
+    const existingUser = await UserRepository.findUserByEmail(data.email);
+    if (existingUser) {
+      throw new Error("Email is already in use");
     }
-    async signIn({ email, password }: { email: string; password: string }) {
+    const hashedPassword: string = await hashPassword(data.password);
+    const newUser = await UserRepository.createUser(data, hashedPassword);
+    const verificationToken = newUser.getVerificationToken();
+    await newUser.save();
+    const verificationLink = `${process.env.NEXT_PUBLIC_URL}/verify-email?verifyToken=${verificationToken}&id=${newUser?._id}`;
+    const message = verificationEmailTemplate(verificationLink);
 
-        const user = await UserRepository.findUserByEmail(email);
+    await sendEmail(newUser?.email, "Email Verification", message);
+    return { user: newUser };
+  }
 
-        if (!user) {
-            throw new Error("Invalid email or password");
-        }
+  async signIn({ email, password }: { email: string; password: string }) {
+    const user = await UserRepository.findUserByEmail(email);
 
-
-        if (!user.isVerified) {
-            throw new Error("Please verify your email before signing in");
-        }
-
-
-        const isPasswordValid = await comparePassword(password, user.password);
-
-        if (!isPasswordValid) {
-            throw new Error("Invalid email or password");
-        }
-
-
-        const token = jwt.sign(
-            { id: user._id, email: user.email },
-            process.env.JWT_SECRET || "your_jwt_secret",
-            { expiresIn: "1d" }
-        );
-
-
-        const userWithoutSensitiveInfo = {
-            _id: user._id,
-            name: user.name,
-            email: user.email,
-            isVerified: user.isVerified
-
-        };
-
-        return { user: userWithoutSensitiveInfo, token };
+    if (!user) {
+      throw new Error("Invalid email or password");
     }
 
-    async verifyEmail(userId: string, verificationToken: string) {
-        const verifyToken = crypto
-            .createHash("sha256")
-            .update(verificationToken)
-            .digest("hex");
-        const user = await UserRepository.findUserByVerificationToken(userId, verifyToken);
-        if (!user) {
-            throw new Error("user not found, token not found, or token expired")
-        }
-        await UserRepository.verifyUser(user);
-        return { message: "success" };
+    if (!user.isVerified) {
+      throw new Error("Please verify your email before signing in");
     }
 
+    const isPasswordValid = await comparePassword(password, user.password);
+
+    if (!isPasswordValid) {
+      throw new Error("Invalid email or password");
+    }
+
+    return user;
+  }
+
+  async verifyEmail(userId: string, verificationToken: string) {
+    const verifyToken = crypto
+      .createHash("sha256")
+      .update(verificationToken)
+      .digest("hex");
+    const user = await UserRepository.findUserByVerificationToken(
+      userId,
+      verifyToken
+    );
+    if (!user) {
+      throw new Error("user not found, token not found, or token expired");
+    }
+    await UserRepository.verifyUser(user);
+    return { message: "success" };
+  }
 }
 
 export default new AuthService();
