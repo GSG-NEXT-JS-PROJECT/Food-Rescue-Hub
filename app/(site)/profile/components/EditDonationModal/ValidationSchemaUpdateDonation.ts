5012cd704095c428973c8014acf230ba1271090a
m7md-Y-3mra <kingmoha2002@gmail.com>
2025-04-23 15:20:35 +0300

    fix: prevent 'claimed' status from being selected when updating a donation

--- app/(site)/profile/components/EditDonationModal/ValidationSchemaUpdateDonation.ts
+++ app/(site)/profile/components/EditDonationModal/ValidationSchemaUpdateDonation.ts
@@ -5,6 +5,11 @@ import { DonationStatus } from "@/@types";
 export const validationSchemaUpdateDonation = Yup.object().shape({
   ...Object.fromEntries(Object.entries(validationSchemaPostDonation.fields)),
   status: Yup.string()
-    .oneOf(Object.values(DonationStatus), "Invalid status value")
+    .oneOf(
+      Object.values(DonationStatus).filter(
+        (status) => status !== DonationStatus.Claimed
+      ),
+      "Invalid status value"
+    )
     .required("Status is required"),
 });
